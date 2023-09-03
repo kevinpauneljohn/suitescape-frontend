cee9c9c1c86c3ff1e1b9360313bf312b16c8b0c2
melmatx <melpalana13@gmail.com>
2023-09-03 19:53:27 +0800

    Hide Splash Screen with React Navigation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import MainNavigation from './navigation/MainNavigation';
+import BootSplash from 'react-native-bootsplash';
 
 const App = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer onReady={() => BootSplash.hide()}>
       <MainNavigation />
     </NavigationContainer>
   );
