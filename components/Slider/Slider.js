c2529ba516ecbbfa526e90441abbb8dfbce4283f
melmatx <melpalana13@gmail.com>
2023-09-04 15:08:48 +0800

    Added documentation for slider

--- components/Slider/Slider.js
+++ components/Slider/Slider.js
@@ -8,25 +8,32 @@ import {
   View,
 } from 'react-native';
 import {Routes} from '../../navigation/Routes';
+import style from './SliderStyles';
 import Dots from '../Dots/Dots';
 import Button from '../Button/Button';
 import SliderItem from '../SliderItem/SliderItem';
-import style from './SliderStyles';
 import BackButton from '../BackButton/BackButton';
 import SignInButton from '../SignInButton/SignInButton';
 import SkipButton from '../SkipButton/SkipButton';
 
 const Slider = ({navigation}) => {
+  // State to keep track of current slide index.
   const [index, setIndex] = useState(0);
+
+  // Reference to the FlatList.
   const flatListRef = useRef(null);
+
+  // Determine if the last slide has been reached.
   const endReached = index === slides.length - 1;
 
+  // Get pixel ratio for DPI-based rendering.
   const pixelRatio = PixelRatio.get();
   const higherDPI = pixelRatio > 2;
   const lowerDPI = pixelRatio <= 2;
 
   const {width} = useWindowDimensions();
 
+  // Navigation header setup
   useEffect(() => {
     navigation.setOptions({
       headerShown: true,
@@ -35,6 +42,7 @@ const Slider = ({navigation}) => {
     });
   }, [navigation]);
 
+  // Function to render the header's right button based on the slide index.
   const renderHeaderRight = useCallback(() => {
     console.log('Render header right');
     if (index === 0) {
@@ -46,6 +54,7 @@ const Slider = ({navigation}) => {
     return null;
   }, [endReached, index, lowerDPI, navigation]);
 
+  // Function to render the header's left button based on the slide index.
   const renderHeaderLeft = useCallback(() => {
     console.log('Render header left');
     if (index > 0) {
@@ -56,6 +65,7 @@ const Slider = ({navigation}) => {
     return null;
   }, [index]);
 
+  // Update the header whenever the slide index changes.
   useEffect(() => {
     console.log('Render header');
     navigation.setOptions({
@@ -63,9 +73,11 @@ const Slider = ({navigation}) => {
       headerLeft: renderHeaderLeft,
     });
 
+    // Scroll to the current index.
     flatListRef.current.scrollToIndex({index, animated: true});
   }, [index, navigation, renderHeaderLeft, renderHeaderRight]);
 
+  // Handler for the 'Next' button click.
   const handleNextButtonClick = () => {
     if (endReached) {
       navigation.replace(Routes.Home);
