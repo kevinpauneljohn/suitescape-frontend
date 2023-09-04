60b9f77549d8a34728faf4c29d72f91008966ffc
melmatx <melpalana13@gmail.com>
2023-09-04 15:00:02 +0800

    React Navigation Setup

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Routes} from './Routes';
 import Home from '../screens/Home';
+import Onboarding from '../screens/Onboarding';
 
 const Stack = createStackNavigator();
 
 const MainNavigation = () => (
-  <Stack.Navigator>
+  <Stack.Navigator
+    initialRouteName={Routes.Onboarding}
+    screenOptions={{headerShown: false}}>
     <Stack.Screen name={Routes.Home} component={Home} />
+    <Stack.Screen name={Routes.Onboarding} component={Onboarding} />
   </Stack.Navigator>
 );
 
