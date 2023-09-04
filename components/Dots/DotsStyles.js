f937d7a144b52709be3f1f7227110e37839caff3
melmatx <melpalana13@gmail.com>
2023-09-04 15:01:44 +0800

    Created Dots component for Slider

---
+++ components/Dots/DotsStyles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+  dot: {
+    width: 15,
+    height: 15,
+    margin: 5,
+    borderRadius: 10,
+  },
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingBottom: 30,
+  },
+});
+
+export default style;
