185d320775d23a0590173eaabdf90a3e9b839706
melmatx <melpalana13@gmail.com>
2023-09-04 15:00:17 +0800

    Created Back Button component

---
+++ components/BackButton/BackButtonStyles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+  backButton: {
+    marginTop: 10,
+    marginLeft: 15,
+    padding: 5,
+  },
+});
+
+export default style;
