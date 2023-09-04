c3756b41aa67e40cde3d0a459bd45bae80952fe8
melmatx <melpalana13@gmail.com>
2023-09-04 14:58:15 +0800

    Imported onboarding data

--- data/onboardingData.js
+++ data/onboardingData.js
@@ -1,16 +1,17 @@
-const data = [
+export default [
   {
+    id: '1',
     title: 'Find a place to travel book a timeslot.',
     img: require('../assets/images/onboarding/page1.png'),
   },
   {
+    id: '2',
     title: 'With this app, you can book anytime Anywhere.',
     img: require('../assets/images/onboarding/page2.png'),
   },
   {
+    id: '3',
     title: 'Let’s Get Started',
     img: require('../assets/images/onboarding/page3.png'),
   },
-].map((item, index) => ({...item, key: index}));
-
-export default data;
+];
