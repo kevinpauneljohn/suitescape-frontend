5638e9adf04d9567d69d096453314dcae02a70cb
melmatx <melpalana13@gmail.com>
2023-09-04 22:15:40 +0800

    Transferred Logo component to Slider

--- screens/Onboarding.js
+++ screens/Onboarding.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import Logo from '../components/Logo/Logo';
 import Slider from '../components/Slider/Slider';
 
 const Onboarding = ({navigation}) => {
   return (
     <SafeAreaView>
-      <Logo />
       <Slider navigation={navigation} />
     </SafeAreaView>
   );
