61bbd5a8b04bf94bf4c03536861d85e21935c008
melmatx <melpalana13@gmail.com>
2023-09-05 23:56:20 +0800

    Created LogoText component

---
+++ components/LogoText/LogoTextStyles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 16,
+  },
+  logoContainer: {
+    paddingVertical: 20,
+  },
+  text: {
+    fontFamily: 'Roboto',
+    fontSize: 25,
+    color: 'black',
+  },
+});
+
+export default style;
