4fb94bff47a1dda7f607f85b8a96b01682d928d6
melmatx <melpalana13@gmail.com>
2023-09-05 19:21:17 +0800

    Switched onboarding places in MainNavigation

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Routes} from './Routes';
-import Home from '../screens/Home';
-import Onboarding from '../screens/Onboarding';
-import Login from '../screens/Login';
-import SignUp from '../screens/SignUp';
+import Home from '../screens/Home/Home';
+import Onboarding from '../screens/Onboarding/Onboarding';
+import Login from '../screens/Login/Login';
+import SignUp from '../screens/SignUp/SignUp';
 
 const Stack = createStackNavigator();
 
@@ -12,8 +12,8 @@ const MainNavigation = () => (
   <Stack.Navigator
     initialRouteName={Routes.Onboarding}
     screenOptions={{headerShown: false}}>
-    <Stack.Screen name={Routes.Home} component={Home} />
     <Stack.Screen name={Routes.Onboarding} component={Onboarding} />
+    <Stack.Screen name={Routes.Home} component={Home} />
     <Stack.Screen name={Routes.Login} component={Login} />
     <Stack.Screen name={Routes.SignUp} component={SignUp} />
   </Stack.Navigator>
