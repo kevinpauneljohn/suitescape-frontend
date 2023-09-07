34994d8061fa26e2dd3bd8bf9dd6304c7f835ef0
melmatx <melpalana13@gmail.com>
2023-09-07 11:50:25 +0800

    Created LogoView component

---
+++ components/LogoView/LogoView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+import Logo from '../../assets/svgs/logo-only.svg';
+import style from './LogoViewStyles';
+
+const LogoView = () => (
+  <View style={style.container}>
+    <Logo width={70} height={70} />
+  </View>
+);
+
+export default LogoView;
