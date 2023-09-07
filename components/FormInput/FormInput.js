af1f0152c8b62b6c75047dafad2504f7b9191963
melmatx <melpalana13@gmail.com>
2023-09-07 11:53:14 +0800

    Used react-native-paper for FormInput component

--- components/FormInput/FormInput.js
+++ components/FormInput/FormInput.js
@@ -1,32 +1,47 @@
 import React, {useState} from 'react';
-import {Pressable, TextInput, View} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {View} from 'react-native';
+import {HelperText, TextInput} from 'react-native-paper';
+import {Colors} from '../../assets/Colors';
 import style from './FormInputStyles';
 
-const FormInput = ({password = false, ...props}) => {
-  const [showPassword, setShowPassword] = useState(false);
+const FormInput = ({
+  password = false,
+  errorMessage = null,
+  placeholder = '',
+  ...props
+}) => {
+  const theme = {
+    colors: {
+      primary: 'gray',
+      secondary: Colors.gray,
+      background: 'white',
+    },
+  };
 
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <View style={style.container}>
       <TextInput
+        theme={theme}
         {...props}
+        mode={'outlined'}
+        label={placeholder}
         secureTextEntry={password && !showPassword}
-        style={{
-          ...style.textInput,
-          ...{paddingVertical: password ? 20 : 16},
-        }}
+        right={
+          password && (
+            <TextInput.Icon
+              icon={showPassword ? 'eye' : 'eye-off'}
+              onPress={() => setShowPassword(!showPassword)}
+            />
+          )
+        }
       />
-      {password && (
-        <Pressable
-          onPress={() => setShowPassword(!showPassword)}
-          style={style.eyeContainer}>
-          {showPassword ? (
-            <Icon name={'eye-slash'} size={20} color={'gray'} />
-          ) : (
-            <Icon name={'eye'} size={20} color={'gray'} />
-          )}
-        </Pressable>
-      )}
+      <HelperText
+        type={'error'}
+        visible={errorMessage}
+        style={errorMessage && style.errorText}>
+        {errorMessage}
+      </HelperText>
     </View>
   );
 };
--- components/FormInput/FormInputStyles.js
+++ components/FormInput/FormInputStyles.js
@@ -1,23 +1,11 @@
 import {StyleSheet} from 'react-native';
-import {Colors} from '../../assets/Colors';
 
 const style = StyleSheet.create({
   container: {
-    borderWidth: 1,
-    borderColor: Colors.gray,
-    borderRadius: 10,
-    marginHorizontal: 28,
-    marginVertical: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    marginHorizontal: 20,
   },
-  textInput: {
-    paddingHorizontal: 25,
-    flex: 1,
-  },
-  eyeContainer: {
-    justifyContent: 'center',
-    paddingHorizontal: 20,
+  errorText: {
+    marginBottom: 4,
   },
 });
 
