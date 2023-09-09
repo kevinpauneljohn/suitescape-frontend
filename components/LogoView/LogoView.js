189c72c08e52ccb7e10406b1712a89f6f8d78281
melmatx <melpalana13@gmail.com>
2023-09-09 21:20:25 +0800

    Changed logo import path

--- components/LogoView/LogoView.js
+++ components/LogoView/LogoView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View} from 'react-native';
-import Logo from '../../assets/svgs/logo-only.svg';
+import Logo from '../../assets/images/svgs/logo-only.svg';
 import style from './LogoViewStyles';
 
 const LogoView = () => (
