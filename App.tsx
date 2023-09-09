f75dec7a5b808665e8cee0aa633e4c4d7eea04b5
melmatx <melpalana13@gmail.com>
2023-09-09 23:26:23 +0800

    Removed PaperProvider from App.tsx

--- App.tsx
+++ App.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import MainNavigation from './navigation/MainNavigation';
 import BootSplash from 'react-native-bootsplash';
-import {PaperProvider} from 'react-native-paper';
 
 const App = () => {
   return (
-    <PaperProvider>
-      <NavigationContainer onReady={() => BootSplash.hide()}>
-        <MainNavigation />
-      </NavigationContainer>
-    </PaperProvider>
+    <NavigationContainer onReady={() => BootSplash.hide()}>
+      <MainNavigation />
+    </NavigationContainer>
   );
 };
 
