96e23519eb4e25b5590c2c1901d7e47198890574
melmatx <melpalana13@gmail.com>
2023-09-09 21:21:26 +0800

    Added persistent values for token and showOnboarding

--- screens/Onboarding/Onboarding.js
+++ screens/Onboarding/Onboarding.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native';
 import Slider from '../../components/Slider/Slider';
+import {useSettings} from '../../storage/settingsStorage';
+import {Routes} from '../../navigation/Routes';
 
-const Onboarding = () => {
+const Onboarding = ({navigation}) => {
+  const [showOnboarding, _setShowOnboarding] = useSettings(
+    'showOnboarding',
+    true,
+  );
+  const [token, _setToken] = useSettings('token', '');
+
+  useEffect(() => {
+    // Reset settings for testing (Uncomment and reload the app after)
+    // _setShowOnboarding(true);
+    // _setToken('');
+
+    if (token) {
+      navigation.replace(Routes.Home);
+      return;
+    }
+    if (!showOnboarding) {
+      navigation.replace(Routes.Login);
+    }
+  });
+
   return (
     <SafeAreaView>
       <Slider />
