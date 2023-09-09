ba4e6b907d5951f79ad322d64b203e06ea4edc92
melmatx <melpalana13@gmail.com>
2023-09-09 21:13:34 +0800

    Cleaned up FormInput component

--- components/FormInput/FormInput.js
+++ components/FormInput/FormInput.js
@@ -1,49 +1,114 @@
-import React, {useState} from 'react';
+import React, {useState, memo, forwardRef} from 'react';
 import {View} from 'react-native';
 import {HelperText, TextInput} from 'react-native-paper';
-import {Colors} from '../../assets/Colors';
 import style from './FormInputStyles';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import {Colors} from '../../assets/Colors';
 
-const FormInput = ({
-  password = false,
-  errorMessage = null,
-  placeholder = '',
-  ...props
-}) => {
-  const theme = {
-    colors: {
-      primary: 'gray',
-      secondary: Colors.gray,
-      background: 'white',
-    },
-  };
-
-  const [showPassword, setShowPassword] = useState(false);
-  return (
-    <View style={style.container}>
-      <TextInput
-        theme={theme}
-        {...props}
-        mode={'outlined'}
-        label={placeholder}
-        secureTextEntry={password && !showPassword}
-        right={
-          password && (
-            <TextInput.Icon
-              icon={showPassword ? 'eye' : 'eye-off'}
-              onPress={() => setShowPassword(!showPassword)}
-            />
-          )
-        }
-      />
-      <HelperText
-        type={'error'}
-        visible={errorMessage}
-        style={errorMessage && style.errorText}>
-        {errorMessage}
-      </HelperText>
-    </View>
-  );
+const theme = {
+  colors: {
+    primary: Colors.blue,
+    background: 'white',
+    text: 'black',
+    onSurfaceVariant: 'gray',
+  },
+  roundness: 10,
 };
 
-export default FormInput;
+const FormInput = forwardRef(
+  (
+    {
+      type = 'text', // text, password, date
+      value = null,
+      onChangeText = null,
+      placeholder = '',
+      errorMessage = null,
+      ...props
+    },
+    ref,
+  ) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const [showDatepicker, setShowDatepicker] = useState(false);
+
+    const handlePasswordPress = () => {
+      setShowPassword(prev => !prev);
+    };
+
+    const handleDatePress = () => {
+      setShowDatepicker(true);
+    };
+
+    const handleDateConfirm = date => {
+      if (!onChangeText) {
+        console.log(
+          'onChangeText is not defined',
+          date.toISOString().split('T')[0],
+        );
+        return;
+      }
+      onChangeText(date.toISOString().split('T')[0]);
+      setShowDatepicker(false);
+    };
+
+    const renderErrorMessages = () => {
+      if (!errorMessage) {
+        return null;
+      }
+
+      return errorMessage.map((message, index) => (
+        <HelperText key={index} type={'error'} visible={message}>
+          {message}
+        </HelperText>
+      ));
+    };
+
+    return (
+      <>
+        <View
+          style={{
+            ...style.inputContainer,
+            ...(errorMessage && {marginBottom: 4}),
+          }}>
+          <TextInput
+            {...props}
+            theme={theme}
+            mode={'outlined'}
+            value={value}
+            onChangeText={onChangeText}
+            label={placeholder}
+            secureTextEntry={type === 'password' && !showPassword}
+            outlineColor={errorMessage ? 'red' : null}
+            ref={ref}
+            right={
+              type !== 'text' && (
+                <TextInput.Icon
+                  icon={
+                    type === 'password'
+                      ? showPassword
+                        ? 'eye-off'
+                        : 'eye'
+                      : 'calendar'
+                  }
+                  onPress={
+                    type === 'password' ? handlePasswordPress : handleDatePress
+                  }
+                />
+              )
+            }
+          />
+          {renderErrorMessages()}
+        </View>
+        {type === 'date' && showDatepicker && (
+          <DateTimePickerModal
+            mode={'date'}
+            isVisible={showDatepicker}
+            onCancel={() => setShowDatepicker(false)}
+            onConfirm={handleDateConfirm}
+          />
+        )}
+      </>
+    );
+  },
+);
+
+export default memo(FormInput);
