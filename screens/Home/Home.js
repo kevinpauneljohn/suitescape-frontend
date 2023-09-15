7372779ac7e9bb57cd673c73eeac0ad24d2dc283
melmatx <melpalana13@gmail.com>
2023-09-15 13:32:41 +0800

    Cleaned up Home Screen

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,15 +1,13 @@
 import React, {useCallback, useRef, useState} from 'react';
-import {
-  FlatList,
-  Pressable,
-  StatusBar,
-  useWindowDimensions,
-  View,
-} from 'react-native';
-import Video from 'react-native-video';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {FlatList, StatusBar, useWindowDimensions, View} from 'react-native';
 import {useFocusEffect} from '@react-navigation/native';
 import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+import VideoItem from '../../components/VideoItem/VideoItem';
+import style from './HomeStyles';
+
+const VIEWABILITY_CONFIG = {
+  itemVisiblePercentThreshold: 90,
+};
 
 const Home = () => {
   const videos = Array(5)
@@ -41,67 +39,37 @@ const Home = () => {
     }, [lastPlayedIndex]),
   );
 
+  const handleViewableItemsChanged = useCallback(({viewableItems}) => {
+    const firstViewableItem = viewableItems[0];
+    if (firstViewableItem?.isViewable) {
+      const newIndex = firstViewableItem.item.id - 1;
+      setLastPlayedIndex(newIndex);
+      setIndex(newIndex);
+    }
+  }, []);
+
   const viewabilityConfigCallbackPairs = useRef([
     {
-      viewabilityConfig: {
-        itemVisiblePercentThreshold: 90,
-      },
-      onViewableItemsChanged: useCallback(({viewableItems}) => {
-        if (viewableItems.length > 0) {
-          const firstViewableItem = viewableItems[0];
-          if (firstViewableItem.isViewable) {
-            const newIndex = firstViewableItem.item.id - 1;
-            setIndex(newIndex);
-            setLastPlayedIndex(newIndex);
-          }
-        }
-      }, []),
+      viewabilityConfig: VIEWABILITY_CONFIG,
+      onViewableItemsChanged: handleViewableItemsChanged,
     },
   ]);
 
   return (
-    <View style={{backgroundColor: 'black'}}>
+    <View style={style.mainContainer}>
       <FlatList
         data={videos}
         keyExtractor={item => item.id.toString()}
-        renderItem={({item}) => {
-          const paused = isPaused || index === null || index !== item.id - 1;
-          return (
-            <Pressable
-              onPress={() => {
-                setIsPaused(prevState => !prevState);
-              }}>
-              {isPaused && (
-                <View
-                  style={{
-                    position: 'absolute',
-                    top: 0,
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    zIndex: 1,
-                  }}>
-                  <Icon
-                    name="pause"
-                    size={50}
-                    color="white"
-                    style={{opacity: 0.8}}
-                  />
-                </View>
-              )}
-              <Video
-                source={item.url}
-                resizeMode={'cover'}
-                paused={paused}
-                loop={true}
-                onError={e => console.log(e)}
-                style={{width, height}}
-              />
-            </Pressable>
-          );
-        }}
+        renderItem={({item}) => (
+          <VideoItem
+            index={index}
+            item={item}
+            isPaused={isPaused}
+            togglePause={() => setIsPaused(prevState => !prevState)}
+            width={width}
+            height={height}
+          />
+        )}
         contentContainerStyle={{marginBottom: bottomTabHeight}}
         showsVerticalScrollIndicator={false}
         snapToInterval={height}
---
+++ screens/Home/HomeStyles.js
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+  mainContainer: {
+    backgroundColor: 'black',
+  },
+});
+
+export default style;
