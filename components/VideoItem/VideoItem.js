d7623059ab92a0ec3a28f20e37b4678c38152f41
melmatx <melpalana13@gmail.com>
2023-09-17 18:09:10 +0800

    Cleaned up Video Item component

--- components/VideoItem/VideoItem.js
+++ components/VideoItem/VideoItem.js
@@ -1,33 +1,76 @@
-import React from 'react';
+import React, {memo, useEffect, useRef} from 'react';
 import {Pressable, View} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Video from 'react-native-video';
 import style from './VideoItemStyles';
+import {baseURL} from '../../services/SuitescapeAPI';
+import {userStorage} from '../../storage/userStorage';
+import useAppState from '../../hooks/useAppState';
+import VideoItemDetails from '../VideoItemDetails/VideoItemDetails';
+import VideoItemIconView from '../VideoItemIconView/VideoItemIconView';
 
-const VideoItem = ({index, item, isPaused, togglePause, width, height}) => {
-  const paused = isPaused || index === null || index !== item.id - 1;
+const VideoItem = ({
+  item,
+  notInFocus,
+  isClickPaused,
+  setIsClickPaused,
+  width,
+  height,
+}) => {
+  const videoRef = useRef(null);
+  const appState = useAppState();
+  const inBackground = !!appState.match(/inactive|background/);
+  const paused = isClickPaused || inBackground || notInFocus;
+
+  const token = userStorage.getString('token');
+
+  useEffect(() => {
+    if (!isClickPaused && notInFocus) {
+      videoRef.current.seek(0);
+    }
+  }, [isClickPaused, notInFocus]);
+
+  const togglePause = () => {
+    setIsClickPaused(prevState => !prevState);
+  };
+
   return (
-    <Pressable onPress={togglePause}>
-      {isPaused && (
-        <View style={style.pauseContainer}>
-          <Icon
-            name="pause"
-            size={50}
-            color="white"
-            style={style.pauseButton}
-          />
-        </View>
-      )}
-      <Video
-        source={item.url}
-        resizeMode={'cover'}
-        paused={paused}
-        loop={true}
-        onError={e => console.log(e)}
-        style={{width, height}}
+    <View>
+      <Pressable onPress={togglePause}>
+        {isClickPaused && (
+          <View style={style.pauseContainer}>
+            <Icon
+              name="play"
+              size={50}
+              color="white"
+              style={style.pauseButton}
+            />
+          </View>
+        )}
+        <Video
+          ref={videoRef}
+          source={{
+            uri: `${baseURL}/videos/${item.id}`,
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }}
+          resizeMode={'cover'}
+          paused={paused}
+          repeat={true}
+          style={{width, height}}
+          onError={e => console.error(e)}
+        />
+      </Pressable>
+      <VideoItemDetails
+        name={'DreamHome'}
+        stars={4}
+        location={'Baguio City, Philippines'}
+        price={'3000'}
       />
-    </Pressable>
+      <VideoItemIconView likes={'10.1k'} />
+    </View>
   );
 };
 
-export default VideoItem;
+export default memo(VideoItem);
