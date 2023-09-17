d6220e4d495115eaedbb4964d7536eb1495ed11d
melmatx <melpalana13@gmail.com>
2023-09-17 20:37:08 +0800

    Added Star Rating widget to VideoItemDetails component

--- components/VideoItemDetails/VideoItemDetails.js
+++ components/VideoItemDetails/VideoItemDetails.js
@@ -1,13 +1,28 @@
 import React, {memo} from 'react';
 import {Text, View} from 'react-native';
 import style from './VideoItemDetailsStyles';
+import StarRating from 'react-native-star-rating-widget';
 
-const VideoItemDetails = ({name, location, stars, price}) => {
+const VideoItemDetails = ({name, location, rating, price}) => {
   return (
     <View style={style.container}>
       <Text style={{...style.text, ...style.bold}}>{name}</Text>
-      <Text style={{...style.text, ...style.smallText}}>{location}</Text>
-      <Text style={{...style.text, ...style.largeText}}>
+      <View style={style.ratingContainer}>
+        <View style={style.starRatingContainer}>
+          <StarRating
+            rating={rating}
+            onChange={() => {}}
+            enableSwiping={false}
+            enableHalfStar={true}
+            starSize={20}
+            animationConfig={{scale: 1}}
+            starStyle={style.starRating}
+          />
+        </View>
+        <Text style={{...style.text, ...style.ratingText}}>{rating}</Text>
+      </View>
+      <Text style={{...style.text, ...style.locationText}}>{location}</Text>
+      <Text style={{...style.text, ...style.priceText}}>
         P{price} Per night
       </Text>
     </View>
