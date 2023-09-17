4ae00ef150f0db5a801333fc9842dd4ffd314e59
melmatx <melpalana13@gmail.com>
2023-09-17 18:08:40 +0800

    Created useAppState custom hook

---
+++ hooks/useAppState.js
@@ -0,0 +1,17 @@
+import {useEffect, useState} from 'react';
+import {AppState} from 'react-native';
+
+const useAppState = () => {
+  const [appState, setAppState] = useState(AppState.currentState);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', nextAppState => {
+      setAppState(nextAppState);
+    });
+    return () => subscription.remove();
+  }, []);
+
+  return appState;
+};
+
+export default useAppState;
