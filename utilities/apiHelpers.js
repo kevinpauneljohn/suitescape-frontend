3193279a5da839058ab36c359dc526aa135e2cb9
melmatx <melpalana13@gmail.com>
2023-09-17 18:04:56 +0800

    Modified apiHelpers.js

--- utilities/apiHelpers.js
+++ utilities/apiHelpers.js
@@ -4,22 +4,25 @@ import {userStorage} from '../storage/userStorage';
 export const handleApiResponse = ({response, onError, onSuccess}) => {
   if (response.data.errors) {
     Alert.alert('Error', response.data.message);
-    onError(response.data.errors);
+    onError && onError(response.data);
     return;
   }
-  Alert.alert('Success', response.data.message);
+  if (response.data.message) {
+    Alert.alert('Success', response.data.message);
+  }
   if (response.data.token) {
     userStorage.setString('token', response.data.token);
   }
   if (response.data.user) {
     console.log(response.data.user);
   }
-  onSuccess && onSuccess();
+  onSuccess && onSuccess(response.data);
+  // console.log(response.data);
 };
 
-export const handleApiError = (err, setErrors) => {
+export const handleApiError = (err, handleErrors) => {
   if (err.response) {
-    setErrors(err.response.data.errors);
+    handleErrors && handleErrors(err.response.data);
     console.log(err.response.data);
   } else {
     Alert.alert('Error', err.message);
