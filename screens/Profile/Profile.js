98b9cb781708fbe517436a73b1be135f7ec31a0c
melmatx <melpalana13@gmail.com>
2023-09-19 00:36:43 +0800

    Added logout functionality on Profile screen

--- screens/Profile/Profile.js
+++ screens/Profile/Profile.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import {Button} from 'react-native-paper';
+import SuitescapeAPI from '../../services/SuitescapeAPI';
+import {useUser} from '../../storage/userStorage';
+import {handleApiError, handleApiResponse} from '../../utilities/apiHelpers';
+import {Alert} from 'react-native';
+import {Routes} from '../../navigation/Routes';
 
-const Profile = () => {
-  return null;
+const Profile = ({navigation}) => {
+  const [token, setToken] = useUser('token', '');
+
+  const logout = async () => {
+    try {
+      const response = await SuitescapeAPI.post(
+        '/logout',
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      handleApiResponse({
+        response,
+        onError: e => Alert.alert('Error', e.message),
+        onSuccess: () => {
+          setToken('');
+          navigation.replace(Routes.LOGIN);
+        },
+      });
+    } catch (err) {
+      handleApiError(err, e => Alert.alert('Error', e.message));
+    }
+  };
+
+  return <Button onPress={() => logout()}>Logout</Button>;
 };
 
 export default Profile;
