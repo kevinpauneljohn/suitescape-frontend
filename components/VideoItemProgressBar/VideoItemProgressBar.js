f4693722e0075972b816f1a2b4b7fd91f8a55da7
melmatx <melpalana13@gmail.com>
2023-09-22 10:35:38 +0800

    Created VideoItemProgressBar component

---
+++ components/VideoItemProgressBar/VideoItemProgressBar.js
@@ -0,0 +1,55 @@
+import React, {useRef, useState} from 'react';
+import {View} from 'react-native';
+import {Slider} from '@miblanchard/react-native-slider';
+import style from './VideoItemProgressBarStyles';
+
+const VideoItemProgressBar = ({progress, duration, setIsPaused, videoRef}) => {
+  const [isSeeking, setIsSeeking] = useState(false);
+  const timeoutRef = useRef(null);
+
+  const handleOnSeek = () => {
+    setIsSeeking(true);
+
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      setIsSeeking(false);
+    }, 1500);
+  };
+
+  const onSlidingStart = () => {
+    setIsPaused(true);
+  };
+
+  const onSlidingComplete = () => {
+    setIsPaused(false);
+  };
+
+  return (
+    <View style={style.mainContainer}>
+      <Slider
+        value={progress}
+        onValueChange={val => {
+          handleOnSeek();
+          videoRef.current.seek(val[0]);
+        }}
+        onSlidingStart={onSlidingStart}
+        onSlidingComplete={onSlidingComplete}
+        maximumValue={duration}
+        minimumTrackTintColor={undefined}
+        maximumTrackTintColor={'white'}
+        trackStyle={style.track}
+        thumbStyle={isSeeking ? style.thumb : style.noThumb}
+        containerStyle={style.sliderContainer}
+        trackMarks={[duration]}
+        renderTrackMarkComponent={() => {
+          return <View style={style.trackMark} />;
+        }}
+      />
+    </View>
+  );
+};
+
+export default VideoItemProgressBar;
