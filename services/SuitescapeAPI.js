fb56c8bac3ae190b63779486c90f5eb02e835416
melmatx <melpalana13@gmail.com>
2023-09-23 10:02:43 +0800

    Cleanup for SuitescapeAPI.js

--- services/SuitescapeAPI.js
+++ services/SuitescapeAPI.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-export const baseURL = 'http://192.168.100.151/suitescape-api.test/api';
+const serverIpAddress = '192.168.100.151';
+
+export const baseURL = `http://${serverIpAddress}/suitescape-api.test/api`;
 
 const SuitescapeAPI = axios.create({
   baseURL,
