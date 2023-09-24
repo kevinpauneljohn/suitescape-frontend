82c3f658d4b89660f0d19817181e05ce4c9868f7
melmatx <melpalana13@gmail.com>
2023-09-24 14:20:03 +0800

    Cleanup for SuitescapeAPI due to the use of video url instead of filename in backend

--- services/SuitescapeAPI.js
+++ services/SuitescapeAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const serverIpAddress = '192.168.100.151';
 
-export const baseURL = `http://${serverIpAddress}/suitescape-api.test/api`;
+const baseURL = `http://${serverIpAddress}/suitescape-api.test/api`;
 
 const SuitescapeAPI = axios.create({
   baseURL,
