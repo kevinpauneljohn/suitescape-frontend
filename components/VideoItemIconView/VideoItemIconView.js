dd2e787e56caece23aeb37b21b7cdf41dfa93e81
melmatx <melpalana13@gmail.com>
2023-09-24 14:22:25 +0800

    Created like and saving functionality, as well as cleanup for showing modal

--- components/VideoItemIconView/VideoItemIconView.js
+++ components/VideoItemIconView/VideoItemIconView.js
@@ -1,23 +1,78 @@
 import React, {memo, useState} from 'react';
-import {View} from 'react-native';
+import {Alert, View} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Foundation from 'react-native-vector-icons/Foundation';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import style from './VideoItemIconViewStyles';
 import VideoItemIcon from '../VideoItemIcon/VideoItemIcon';
+import SuitescapeAPI from '../../services/SuitescapeAPI';
+import {userStorage} from '../../storage/userStorage';
+import {handleApiError, handleApiResponse} from '../../utilities/apiHelpers';
 
-const VideoItemIconView = ({likes, setShowModal}) => {
-  const [isLiked, setIsLiked] = useState(false);
-  const [isSaved, setIsSaved] = useState(false);
+const VideoItemIconView = ({
+  id,
+  likes,
+  isVideoLiked,
+  isVideoSaved,
+  setShowModal,
+}) => {
+  const [videoLikes, setVideoLikes] = useState(likes);
+  const [isLiked, setIsLiked] = useState(isVideoLiked);
+  const [isSaved, setIsSaved] = useState(isVideoSaved);
+
+  const token = userStorage.getString('token');
+
+  const handleLike = () => {
+    SuitescapeAPI.post(
+      `/videos/${id}/like`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+      .then(response =>
+        handleApiResponse({
+          response,
+          onSuccess: ({liked}) => {
+            setIsLiked(liked);
+            setVideoLikes(liked ? videoLikes + 1 : videoLikes - 1);
+          },
+        }),
+      )
+      .catch(err => handleApiError(err));
+  };
+
+  const handleSave = () => {
+    SuitescapeAPI.post(
+      `/videos/${id}/save`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+      .then(res =>
+        handleApiResponse({
+          response: res,
+          onSuccess: ({saved}) => {
+            setIsSaved(saved);
+          },
+        }),
+      )
+      .catch(err => handleApiError(err));
+  };
 
   const iconsConfig = [
     {
       IconComponent: AntDesign,
       name: 'heart',
-      label: likes,
+      label: videoLikes,
       color: isLiked ? 'red' : 'white',
-      onPress: () => setIsLiked(prevState => !prevState),
+      onPress: handleLike,
     },
     {
       IconComponent: AntDesign,
@@ -36,13 +91,13 @@ const VideoItemIconView = ({likes, setShowModal}) => {
       name: 'bookmark',
       label: 'Save',
       color: isSaved ? 'gold' : 'white',
-      onPress: () => setIsSaved(prevState => !prevState),
+      onPress: handleSave,
     },
     {
       IconComponent: MaterialCommunityIcons,
       name: 'format-list-bulleted-type',
       label: 'Section',
-      onPress: () => setShowModal(true),
+      onPress: setShowModal,
     },
   ];
 
