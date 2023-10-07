fcfd117f97cc142aea7d003e3b313ea965a75001
melmatx <melpalana13@gmail.com>
2023-10-07 13:51:16 +0800

    Added new global styles

--- assets/styles/globalStyles.js
+++ assets/styles/globalStyles.js
@@ -1,8 +1,15 @@
 import {StyleSheet} from 'react-native';
 
 const globalStyles = StyleSheet.create({
+  flexFull: {
+    flex: 1,
+  },
   registrationButtonContainer: {
     marginTop: 12,
+    marginHorizontal: 23,
+  },
+  emptyText: {
+    marginTop: 3,
   },
 });
 
@@ -10,5 +17,7 @@ export const pressedOpacity = (pressed, opacity = 0.5) =>
   pressed ? {opacity} : {};
 export const disabledOpacity = (disabled, opacity = 0.6) =>
   disabled ? {opacity} : {};
+export const pressedBgColor = (pressed, backgroundColor = 'lightgray') =>
+  pressed ? {backgroundColor} : {};
 
 export default globalStyles;
