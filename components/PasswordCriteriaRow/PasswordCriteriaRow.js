2fa0247e5d419291d8aa673a4af6eac9a7c7b200
melmatx <melpalana13@gmail.com>
2023-10-07 13:45:13 +0800

    Renamed CriteriaRow to PasswordCriteriaRow

---
+++ components/PasswordCriteriaRow/PasswordCriteriaRow.js
@@ -0,0 +1,27 @@
+import React, {memo} from 'react';
+import {Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import style from './PasswordCriteriaRowStyles';
+
+const PasswordCriteriaRow = ({met, text}) => {
+  return (
+    <View style={style.container}>
+      <Icon
+        name={met ? 'checkmark' : 'alert-circle'}
+        size={15}
+        color={met ? 'green' : 'red'}
+      />
+      <View style={style.textContainer}>
+        <Text
+          style={{
+            ...style.text,
+            ...{color: met ? 'green' : 'red'},
+          }}>
+          {text}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default memo(PasswordCriteriaRow);
---
+++ components/PasswordCriteriaRow/PasswordCriteriaRowStyles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  textContainer: {
+    marginLeft: 5,
+  },
+  text: {
+    fontFamily: 'Roboto',
+    fontSize: 12,
+  },
+});
+
+export default style;
