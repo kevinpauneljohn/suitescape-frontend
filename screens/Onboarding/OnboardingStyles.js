48e43cdef238ff1da66cb77715d153a027b5f7de
melmatx <melpalana13@gmail.com>
2023-10-07 13:47:13 +0800

    Created Onboarding screen styles

---
+++ screens/Onboarding/OnboardingStyles.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+  dot: {
+    width: 15,
+    height: 15,
+    margin: 4,
+    borderRadius: 10,
+  },
+  dotContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  nextButtonContainer: {
+    marginTop: 30,
+    marginHorizontal: 24,
+  },
+});
+
+export default style;
