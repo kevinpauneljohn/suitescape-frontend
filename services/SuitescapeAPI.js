7be5207070514cd57006fd866395352340fc7ebe
melmatx <melpalana13@gmail.com>
2023-10-07 13:37:36 +0800

    Added comments for SuitescapeAPI

--- services/SuitescapeAPI.js
+++ services/SuitescapeAPI.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+// Replace this with your local/private IP Address
+// const serverIpAddress = '192.168.1.142';
 const serverIpAddress = '192.168.100.151';
 
 export const baseURL = `http://${serverIpAddress}/suitescape-api.test/api`;
