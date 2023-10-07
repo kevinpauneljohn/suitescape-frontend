1c8fd04d281a4420f04d895c616c752b1e649252
melmatx <melpalana13@gmail.com>
2023-10-07 13:46:53 +0800

    Created places nearby data

---
+++ data/placesNearbyData.js
@@ -0,0 +1,35 @@
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
+
+export default {
+  restaurant: {
+    name: 'Restaurant',
+    icon: 'restaurant',
+    iconSize: 25,
+    IconLibrary: MaterialIcons,
+  },
+  mall: {
+    name: 'Mall',
+    icon: 'building',
+    iconSize: 25,
+    IconLibrary: FontAwesome6,
+  },
+  hospital: {
+    name: 'Hospital',
+    icon: 'hospital',
+    iconSize: 20,
+    IconLibrary: FontAwesome6,
+  },
+  bank: {
+    name: 'Bank',
+    icon: 'building-columns',
+    iconSize: 20,
+    IconLibrary: FontAwesome6,
+  },
+  church: {
+    name: 'Church',
+    icon: 'church',
+    iconSize: 20,
+    IconLibrary: FontAwesome6,
+  },
+};
