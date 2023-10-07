f35c8547f91dedccb51e227448ae0027856b9329
melmatx <melpalana13@gmail.com>
2023-10-07 13:49:19 +0800

    Created AppHeader component styles

---
+++ components/AppHeader/AppHeaderStyles.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingBottom: 5,
+    left: 0,
+    right: 0,
+    backgroundColor: 'white',
+    borderBottomColor: 'lightgray',
+    borderBottomWidth: 1,
+  },
+  text: {
+    marginLeft: 5,
+    fontSize: 16,
+    color: 'black',
+  },
+});
+
+export default style;
